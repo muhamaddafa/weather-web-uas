3e853fb0fe7c477d8709d4e4b31703926d2b86fc
muhamaddafa <gaming102dafa@gmail.com>
2023-05-12 09:47:20 +0700

    Fixing Bug Home Navigation link

--- src/components/MainApp/MenuMobile.jsx
+++ src/components/MainApp/MenuMobile.jsx
@@ -4,10 +4,7 @@ import { Link } from "react-router-dom";
 const MenuMobile = (props) => {
 	return (
 		<div className="hidden menu-mobile pt-3 pb-6 bg-slate-50 dark:bg-gray-800 dark:text-white">
-			<Link
-				to="/UAS-IntroductionToInternetTechnology"
-				className="linkNavItem flex justify-center mb-5"
-			>
+			<Link to="/" className="linkNavItem flex justify-center mb-5">
 				Home
 			</Link>
 			<Link to="/AboutUs" className="linkNavItem flex justify-center mb-5">
